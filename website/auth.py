2db2205e2f6065ea1a07b1f3bb653c5d7de5097b
xs1256 <ooixinsheng12345@gmail.com>
2024-03-09 19:47:42 +0800

    Set up authentication, database, views

--- website/auth.py
+++ website/auth.py
@@ -1,37 +1,65 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        ...
+        email = request.form.get('email')
+        password = request.form.get('password')
 
-    return render_template('login.html')
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category="error")
+        else:
+            flash('Invalid email.', category='error')
+
+    return render_template('login.html', user=current_user)
 
 @auth.route('/logout')
 def logout():
-    return "<p> Logout <p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        first_name = request.form.get('firstName')
+        last_name = request.form.get('lastName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
+        user = User.query.filter_by(email=email).first()
+
+        if user:
+            flash('Email already exists.', category='error')
         # Validate
-        if len(email) < 4:
+        elif len(email) < 4:
             flash('Email must be greater than 3 characters.', category='error')
-        elif len(firstName) < 2:
+        elif len(first_name) < 2:
             flash('First name must be greater than 1 characters.', category='error')
+        elif len(last_name) < 2:
+            flash('Last name must be greater than 1 characters.', category='error')
         elif password1 != password2:
             flash('Password don\'t match.', category='error')
         elif len(password1) < 7:
             flash('Password must be greater than 6 characters.', category='error')
         else:
-            flash('Account created', category='success')
+            new_user = User(email=email, first_name=first_name, last_name=last_name, password=generate_password_hash(password2, method='scrypt'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(user, remember=True)
+            flash('Account created!', category='success')
+            return redirect(url_for('views.home'))
 
-
-    return render_template('register.html')
+    return render_template('register.html', user=current_user)
